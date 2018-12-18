599a1299df68a4660f913e322ee1f28d98f1109f
suecharo <suehiro619@gmail.com>
2018-12-18 23:28:21 +0900

    Create run model

--- NIG-API/NIG-api/dummy_data.py
+++ NIG-API/NIG-api/dummy_data.py
@@ -2,15 +2,24 @@
 
 DUMMY_SERVICE_INFO = {
     # The Workflow engine accepted by API-server and the Workflow definition accepted by that Workflow engine
+    # TODO Is there more than one version
+    # TODO wrong form POST /runs
     "workflow_type_versions": {
         "cwltool": {
             "workflow_type_version": [
-                "CWL",
+                "CWL v1.0.2",
+                "WDL 0.0.5",
+            ],
+        },
+        "toil": {
+            "workflow_type_version": [
+                "CWL v1.0.2",
             ],
         },
     },
     "supported_wes_versions": [
         "1.0.0",
+        "0.2.0",
     ],
     "supported_filesystem_protocols": [
         "http",
@@ -21,6 +30,7 @@ DUMMY_SERVICE_INFO = {
     ],
     "workflow_engine_versions": {
         "cwltool": "1.0.20181201184214",
+        "toil": "3.18.0",
     },
     # Default parameters accepted by the workflow engine
     # TODO The specification here is wrong. Entry for each engine is required.
@@ -32,19 +42,19 @@ DUMMY_SERVICE_INFO = {
     #     },
     # ],
     "system_state_counts": {
-        "UNKNOWN": 0,
-        "QUEUED": 0,
-        "INITIALIZING": 0,
-        "RUNNING": 0,
-        "PAUSED": 0,
-        "COMPLETE": 0,
-        "EXECUTOR_ERROR": 0,
-        "SYSTEM_ERROR": 0,
-        "CANCELED": 0,
+        "UNKNOWN": 3,
+        "QUEUED": 5,
+        "INITIALIZING": 2,
+        "RUNNING": 2,
+        "PAUSED": 5,
+        "COMPLETE": 1,
+        "EXECUTOR_ERROR": 1,
+        "SYSTEM_ERROR": 1,
+        "CANCELED": 1,
         "CANCELING": 0,
     },
-    "auth_instructions_url": "https://github.com/suecharo/NIG",
-    "contact_info_url": "https://github.com/suecharo/NIG",
+    "auth_instructions_url": "dummy_auth_instructions_url",
+    "contact_info_url": "dummy_contact_info_url",
     # TODO Arbitrary parameter set, I'm wondering whether to use this
     # "tags": {
     #     "additionalProp1": "string",
@@ -57,69 +67,102 @@ DUMMY_SERVICE_INFO = {
 DUMMY_RUNS_LIST = {
     "runs": [
         {
-            "run_id": "string",
-            "state": "UNKNOWN"
-        }
+            "run_id": "1",
+            "state": "UNKNOWN",
+        },
+        {
+            "run_id": "2",
+            "state": "RUNNING",
+        },
+        {
+            "run_id": "3",
+            "state": "COMPLETE",
+        },
+        {
+            "run_id": "4",
+            "state": "CANCELED",
+        },
     ],
-    "next_page_token": "string"
+    # Optional
+    "next_page_token": "dummy_next_page_token",
 }
 
 DUMMY_RUNS_RUN = {
-    "run_id": "string"
+    "run_id": "1",
 }
 
 DUMMY_RUNS_INFO = {
-    "run_id": "string",
+    "run_id": "1",
     "request": {
-        "workflow_params": {},
-        "workflow_type": "string",
+        # Json
+        "workflow_params": {
+            "dummy_workflow_param_key_1": "dummy_workflow_param_value_1",
+            "dummy_workflow_param_key_2": "dummy_workflow_param_value_2",
+        },
+        # "workflow_type": "CWL",
         "workflow_type_version": "string",
-        "tags": {
-            "additionalProp1": "string",
-            "additionalProp2": "string",
-            "additionalProp3": "string"
-        },
-        "workflow_engine_parameters": {
-            "additionalProp1": "string",
-            "additionalProp2": "string",
-            "additionalProp3": "string"
-        },
-        "workflow_url": "string"
+        # "tags": {
+        #     "additionalProp1": "string",
+        #     "additionalProp2": "string",
+        #     "additionalProp3": "string"
+        # },
+        # "workflow_engine_parameters": {
+        #     "additionalProp1": "string",
+        #     "additionalProp2": "string",
+        #     "additionalProp3": "string"
+        # },
+        # "workflow_url": "dummy_workflow_url",
+        # TODO There is no Workflow Engine
     },
     "state": "UNKNOWN",
     "run_log": {
-        "name": "string",
+        "name": "dummy_run_name",
         "cmd": [
-            "string"
+            "dummy_run_cmd_1",
+            "dummy_run_cmd_2",
         ],
-        "start_time": "string",
-        "end_time": "string",
-        "stdout": "string",
-        "stderr": "string",
-        "exit_code": 0
+        "start_time": "dummy_run_start_date",
+        "end_time": "dummy_run_end_date",
+        "stdout": "dummy_run_stdout",
+        "stderr": "dummy_run_stderr",
+        "exit_code": 0,
     },
-    "task_logs": [
-        {
-            "name": "string",
-            "cmd": [
-                "string"
-            ],
-            "start_time": "string",
-            "end_time": "string",
-            "stdout": "string",
-            "stderr": "string",
-            "exit_code": 0
-        }
-    ],
-    "outputs": {}
+    # "task_logs": [
+    #     {
+    #         "name": "dummy_task_1_name",
+    #         "cmd": [
+    #             "dummy_task_1_cmd_1",
+    #             "dummy_task_1_cmd_2",
+    #         ],
+    #         "start_time": "dummy_task_1_start_date",
+    #         "end_time": "dummy_task_1_end_date",
+    #         "stdout": "dummy_task_1_stdout",
+    #         "stderr": "dummy_task_1_stderr",
+    #         "exit_code": 0,
+    #     },
+    #     {
+    #         "name": "dummy_task_2_name",
+    #         "cmd": [
+    #             "dummy_task_2_cmd_1",
+    #             "dummy_task_2_cmd_2",
+    #         ],
+    #         "start_time": "dummy_task_2_start_date",
+    #         "end_time": "dummy_task_2_end_date",
+    #         "stdout": "dummy_task_2_stdout",
+    #         "stderr": "dummy_task_2_stderr",
+    #         "exit_code": 0,
+    #     },
+    # ],
+    # TODO
+    # "outputs": {}
 }
 
 DUMMY_RUNS_CANCEL = {
-    "run_id": "string"
+    "run_id": "1",
 }
 
 
 DUMMY_RUNS_STATUS = {
-    "run_id": "string",
-    "state": "UNKNOWN"
+    "run_id": "1",
+    "state": "UNKNOWN",
 }
--- NIG-web/NIG-web/app/models/model_run.py
+++ NIG-web/NIG-web/app/models/model_run.py
@@ -4,17 +4,29 @@ from django.db import models
 from django.utils import timezone
 from django.utils.translation import ugettext_lazy as _
 
+from app.models import Service, WorkflowEngine, WorkflowTypeVersion
 
-class Run(models.Model):
-    name = models.CharField(_("run name"), max_length=256)
+
+class CommonInfo(models.Model):
+    created_at = models.DateTimeField(_("Created date"), default=timezone.now)
+    updated_at = models.DateTimeField(_("Updated date"), auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Run(CommonInfo):
+    name = models.CharField(_("Run name"), max_length=256)
     user = models.ForeignKey(User, verbose_name=_(
-        "executing user"), on_delete=models.CASCADE)
-    workflow = models.CharField(_("workflow name"), max_length=256)
-    status = models.CharField(_("status"), max_length=64)
-    date_created = models.DateTimeField(
-        _("date created"), default=timezone.now)
-    date_updated = models.DateTimeField(_("date updated"), auto_now=True)
-    config_file_path = models.CharField(_("config file path"), max_length=256)
+        "Executing user"), on_delete=models.CASCADE)
+    service = models.ForeignKey(Service, verbose_name=_(
+        "Executing service"), on_delete=models.CASCADE)
+    id_in_service = models.IntegerField(_("ID in service"))
+    status = models.CharField(_("Status"), max_length=64)
+    workflow_engine = models.ForeignKey(WorkflowEngine, verbose_name=_(
+        "Workflow engine"), on_delete=models.CASCADE)
+    workflow_type_version = models.ForeignKey(WorkflowTypeVersion, verbose_name=_(
+        "Workflow type version"), on_delete=models.CASCADE)
 
     class Meta:
         db_table = "run"
@@ -22,4 +34,37 @@ class Run(models.Model):
         verbose_name_plural = "runs"
 
     def __str__(self):
-        return self.name
+        return "Run: {}".format(self.name)
+
+
+class Log(CommonInfo):
+    name = models.CharField(_("Log name"), max_length=256)
+    run = models.ForeignKey(Run, verbose_name=_(
+        "Run"), on_delete=models.CASCADE)
+    start_time = models.DateTimeField(_("Start time"))
+    end_time = models.DateTimeField(_("End time"))
+    stdout = models.TextField(_("Stdout"))
+    stderr = models.TextField(_("Stderr"))
+    exit_code = models.IntegerField(_("Exit code"))
+
+    class Meta:
+        db_table = "log"
+        verbose_name = "log"
+        verbose_name_plural = "logs"
+
+    def __str__(self):
+        return "Log: {}".format(self.name)
+
+
+class Command(CommonInfo):
+    log = models.ForeignKey(Log, verbose_name=_("Log"),
+                            on_delete=models.CASCADE)
+    cmd = models.CharField(_("Command"), max_length=256)
+
+    class Meta:
+        db_table = "command"
+        verbose_name = "command"
+        verbose_name_plural = "commands"
+
+    def __str__(self):
+        return "Command: {}".format(self.id)
