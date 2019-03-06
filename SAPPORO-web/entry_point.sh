d6ecbaecd55ebb33fe0b891e4715c6718e5dbe0e
suecharo <suehiro619@gmail.com>
2019-03-06 15:13:00 +0900

    Fix docker-compose dependency

--- SAPPORO-web/entry_point.sh
+++ SAPPORO-web/entry_point.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-sh /tmp/wait-for-it.sh database:5432
+sh /opt/SAPPORO-web/wait-for database:5432
 python3 /opt/SAPPORO-web/SAPPORO-web/config/generate_local_settings.py
 python3 /opt/SAPPORO-web/SAPPORO-web/manage.py makemigrations app
 python3 /opt/SAPPORO-web/SAPPORO-web/manage.py migrate
-python3 /opt/SAPPORO-web/SAPPORO-web/manage.py collectstatic
+echo yes | python3 /opt/SAPPORO-web/SAPPORO-web/manage.py collectstatic
 uwsgi /opt/SAPPORO-web/config/uwsgi.ini
